accb140f68ca1b5229c1ef3171528e82faf97e5f
kaiweiOvO <kaiweiovo@163.com>
2020-04-06 19:41:47 +0800

    commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let swiper = new Swiper('.swiper-container', {
+        // effect: 'fade',
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 3000
+        }
+    })
+}
